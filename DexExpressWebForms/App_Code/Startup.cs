1d06b00b8b284d0bae8c508e58163d191fb9a212
Tu Tong <ttong@redhawkis.com>
2018-09-17 10:57:18 -0400

    Add project files.

---
+++ DexExpressWebForms/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DexExpressWebForms.Startup))]
+namespace DexExpressWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
